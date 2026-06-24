850451b16eb9766af19eef9492fa5fe05dc86dbb
r05t02 <256002142+r05t02@users.noreply.github.com>
2026-06-24 19:24:47 +0530

    Add Search by Match fixture functionality

--- match.py
+++ match.py
@@ -2,31 +2,71 @@ from datetime import datetime, timezone, timedelta
 from zoneinfo import ZoneInfo
 import json
 import sys
+import requests
+
 def main():
     ## PULL DATA DIRECTLY FROM URL
-    # import requests
-    # url = "https://raw.githubusercontent.com/openfootball/worldcup.json/master/2026/worldcup.json"
-    # match_data = requests.get(url).json()
-    # matches = match_data["matches"]
-    # convert_to_indian_format(matches)
+    url = "https://raw.githubusercontent.com/openfootball/worldcup.json/master/2026/worldcup.json"
+    match_data = requests.get(url).json()
+    matches = match_data["matches"]
+    
     
 
     # READ DATA FROM LOCAL .JSON FILE
-    with open('matches.json','r') as f:
-        match_data = json.load(f)
+    # with open('matches.json','r') as f:
+    #     match_data = json.load(f)
        
         
-    matches = match_data["matches"]
+    # matches = match_data["matches"]
 
     print("------------------------------------------------------------------------------------------------------------------------------------------------------------------")
 
+    
     #Replaces UTC to IST in dates and time
     convert_to_indian_format(matches)
     
+
+    #sorts the matches list, by the using sort_matches as key (which returns a dateime object)
     matches.sort(key=sort_matches)
     
-    for match in matches:
-        print(match)
+
+
+
+
+    query = get_query()
+
+    user_query_type = query_type(query)
+
+    
+
+    
+
+    if user_query_type == "FIXTURE":
+        search_fixture(query, matches)
+    if user_query_type == "DATE":
+       search_match_by_date(query, matches)
+    # if user_query_type == "ROUND":
+        #search_by_round(query):
+    # if user_query_type == "SINGLE TEAM":
+    #      #search_Team_matches(query)
+
+
+    
+ 
+
+
+
+
+    # try:
+    #     fixture = sys.argv[1]
+    #     team_a, team_b =fixture.split(" vs ")
+    # except:
+    #     team = input("Which team? ")
+
+    # for match in matches:
+    #     if team == match["team1"] or team == match["team2"]:
+    #          print("Target found")
+    #          print(match)
    
     # sorted_by_DateTime = sorted(matches, key=sort_matches)
     # print(sorted_by_DateTime)
@@ -92,6 +132,68 @@ def sort_matches(match):
 
 
 
+def get_query():
+    if len(sys.argv) == 1:   #ONLY sys.argv[0] is there which is always name of the file
+        user_inp = input("What do you want to look for? [Team, Fixture, Matches On A Date]  ")
+        return user_inp
+    else:
+        user_inp = ""
+        for x in range(len(sys.argv)):
+            if x == 0:
+                continue
+            user_inp += sys.argv[x] + " "
+        return user_inp
+
+        
+def query_type(query):
+    query = query.strip()
+
+    stages = ["Matchday", "Round of 16", "Round of 32", "Quarter-final", "Semi-final", "Final"]
+    
+    if "vs" in query:
+        return "FIXTURE"
+    
+    if "/" in query:
+        return "DATE"
+    
+    for stage in stages:
+            if stage.lower() in query.lower():
+                return "ROUND"
+            
+    return "SINGLE TEAM"
+    
+    
+    
+def search_fixture(query, matches):
+    query = query.strip()
+
+    team_A, team_B = query.split(" vs ")
+
+    print(f"Searching for: '{team_A}' vs '{team_B}'")
+
+   
+
+    for match in matches:
+        if (match['team1'] == team_A or match["team2"] == team_A) and (match["team1"] == team_B or match["team2"] == team_B):
+            print(match)
+            if match.get("score"):
+                print("Match started (LIVE or FINISHED)")
+            else:
+                print("Upcoming match")
+
+
+def search_match_by_date(query, matches):
+    date = query
+    # convert_to_indian_format(matches)
+    date = date.strip()
+
+    for match in matches:
+        if match["date"]  == date:
+            print(match,"\n")
+
+
+
+    
         
           
 
@@ -105,46 +207,57 @@ if __name__ == "__main__":
 
 
 
+# TODO
+# input = sys.argv[1]
 
+# if "vs" in input:
+#     search teams
 
+# elif looks_like_date(input):
+#     search date
 
-
-# date = input("DATE: ")
-
-# for x in range(len(matches)):
-#     if date == matches[x]["date"]:
-#         print(matches[x])
-#         time = matches[x]["time"]
-#         time = time.split(":")
-#         kick_off_HOUR = time[0]
-        
-
-
-
+# elif round_exists(input):
+#     search round
 
 
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-##Implement option to find match using command line
-# fixture = sys.argv[1]
-# fixture = fixture.split(" vs ")
-# print(fixture)
-
-
+#TODO - CLI IMPLEMENTATION 
 ##Implement option to find matches of a particular team using command line
+
+
+#TODO
+# Desired usage:
+
+# python matches.py "Argentina"
+
+# python matches.py "Argentina vs France"
+
+# python matches.py "26/06/2026"
+
+# python matches.py "round of 16"
+
+# python matches.py "semi-final"
+
+
+
+
+
+# Potential future commands:
+
+#python match.py Argentina vs France 11/11/1111 semi
+
+# python matches.py argentina
+
+# python matches.py france
+
+# python matches.py today          ????
+
+# python matches.py next           ????
+
+# python matches.py "group a"
+
 
 
 
